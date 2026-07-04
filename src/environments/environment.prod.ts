f1c91297032c123d03405384c04e51a1c5aa74d0
marc <zwarte336@gmail.com>
2026-07-05 00:07:38 +0200

    hh

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  appVersion: '0.2.0',
   apiUrl: 'https://api.constantine.tours/api',
   stripePublishableKey: 'pk_test_51TJWf4BctjJ9LyNmX96OuslbjzcOeU8A2WWHyZCTA44On5qRa5KNOs8PmKYdgGSbkIhbEaFDdluJ8Z8P8ErWEt6m00DJPNdBre',
   googleClientId: '403080956691-0u8cdisr5kebjm3lgttokt5i0fm80lm2.apps.googleusercontent.com'
